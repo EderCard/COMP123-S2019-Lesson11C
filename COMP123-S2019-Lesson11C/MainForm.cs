e414fe39abc036107aeef64cbc674b8e4708756a
EderCard <edersoncard@gmail.com>
2019-07-22 16:20:35 -0400

    LocalDatabase connection

---
+++ COMP123-S2019-Lesson11C/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Lesson11C
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// This is the event handler for the MainForm Closing event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+        /// <summary>
+        /// This is the event handler for the ExitToolStripMenuItem click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /// <summary>
+        /// This is the event handler for the AboutToolStripItem click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Program.aboutForm.ShowDialog();
+        }
+
+        private void HelpToolStripButton_Click(object sender, EventArgs e)
+        {
+            Program.aboutForm.ShowDialog();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'sectionCDatabaseDataSet.StudentTable' table. You can move, or remove it, as needed.
+            this.studentTableTableAdapter.Fill(this.sectionCDatabaseDataSet.StudentTable);
+
+        }
+
+        private void ShowDataButton_Click(object sender, EventArgs e)
+        {
+            var StudentList =
+                from student in this.sectionCDatabaseDataSet.StudentTable
+                select student;
+
+            foreach (var student in StudentList.ToList())
+            {
+                Debug.WriteLine("Student Id: " + student.StudentId + "Last Name:"+ student.LastName);
+            }
+        }
+    }
+}
